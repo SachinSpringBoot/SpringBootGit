849c8290385a4d111692b417a591804c72b01d10
SachinSpringBoot <sononesachin3012@gmail.com>
2023-10-04 23:06:34 +0530

    print 1 to 10

--- DemoProject/src/com/velocity/model/Test.java
+++ DemoProject/src/com/velocity/model/Test.java
@@ -3,9 +3,10 @@ package com.velocity.model;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello");
-
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
